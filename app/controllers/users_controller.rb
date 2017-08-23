6c30773b04466c68253d0b0688ea28cecc1ed230
hoanghuy <denhat0321@gmail.com>
2017-08-23 16:27:19 +0700

    chap11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i(new create show)
+  before_action :logged_in_user, only: [:show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   before_action :load_user, only: [:show, :destroy]
@@ -16,9 +16,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t ".welcome"
-      redirect_to @user
+      @user.send_activation_email
+      flash.now[:info] = t ".check_email"
+      render :new
     else
       flash.now[:danger] = t ".failed"
       render :new
